4706baac35047bf1d305da2a0efecfadb895e9c6
ytyaru <yttry0@gmail.com>
2025-09-19 19:57:55 +0900

    add memo

--- memo/split-page/getBoundingClientRect/3/parser.js
+++ memo/split-page/getBoundingClientRect/3/parser.js
@@ -1,17 +1,40 @@
 (function(){// Manuscript > TextBlock > HTML > Element
 class TextBlock {
+    constructor() {this._={}; this._.blocks=[];}
     fromText(text) {
-        if (0===text.trim().length) { return [] }
-        text = text.replace('\r\n', '\n')
-        text = text.replace('\r', '\n')
-        const blocks = []; let start = 0;
+        this._.blocks = [];
+        if (0===text.trim().length) { return this._.blocks }
+        //text = text.replace('\r\n', '\n')
+        //text = text.replace('\r', '\n')
+        //text = text.replace(/\r\n?/gm, '\n').trimLine();
+        text = text.normalizeLineBreaks().trimLine();
+        let start = 0;
         for (let match of text.matchAll(/\n\n/gm)) {
-            blocks.push(text.slice(start, match.index).trimLine())
+            this._.blocks.push(text.slice(start, match.index).trimLine())
             start = match.index + 2
         }
-        blocks.push(text.slice(start).trimLine())
+        this._.blocks.push(text.slice(start).trimLine())
         return blocks.filter(v=>v).map(b=>/^#{1,6} /.test(b) ? b.split('\n') : b).flat();
     }
+    async *generate(text) {
+        this._.blocks = [];
+        if (0===text.trim().length) { return this._.blocks }
+        //text = text.replace(/\r?\n/gm, '\n')
+        //text = text.replace(/\r\n?/gm, '\n').trimLine();
+        text = text.normalizeLineBreaks().trimLine();
+        let start = 0; let block = null;
+        for (let match of text.matchAll(/\n\n/gm)) {
+            block = text.slice(start, match.index).trimLine();
+            this._.blocks.push(block);
+            start = match.index + 2
+            yield block;
+        }
+        block = text.slice(start).trimLine();
+        blocks.push(block);
+        yield block;
+        this._.blocks = blocks.filter(v=>v).map(b=>/^#{1,6} /.test(b) ? b.split('\n') : b).flat();
+    }
+    get blocks() {return this._.blocks}
 }
 const tb = new TextBlock();
 class ManuscriptBlock {
@@ -117,5 +140,62 @@ class JavelParser {
     }
     */
 }
+// https://github.com/nodeca/js-yaml 依存
+class JavelParser {
+    constructor() {
+    }
+    parse(text) {
+        text = text.normalizeLineBreaks().trimLine();
+        return {meta: ,body{blocks:, els:}};
+    }
+    async *generate(text) {
+
+    }
+    #splitMetaBody(text) {
+        const matches = text.matchAll(/^# /);
+        if (0===matches.length) {throw new TypeError(`Javel形式のテキストには一つ以上の見出しが必要です。行頭が# の文字列を含めてください。`)}
+        if (0===matches[0].index) {
+            console.warn('FlontMatterを省略しました。最初の見出しレベル1〜6をタイトル、著者名、キャッチコピーとし、本文をあらすじとします。');
+            const metaLastIndex = (1===matches.length) ? text.length : matches[1].index;
+            if (1===matches.length) {console.warn('本文がありません。');}
+            const coverText = text.slice(0, metaLastIndex);
+            const bodyText = text.slice(metaLastIndex);
+            const parser = new TextBlock();
+            const coverBlocks = parser.fromText(coverText);
+
+            const meta = {javel:{author:{}}, el:{}}
+            let i = coverBlocks.findIndex(b=>b.startsWith('# '));
+            if (-1 < i) {meta.javel.title = coverBlocks[i].slice(2)}
+            i = coverBlocks.findIndex(b=>b.startsWith('## '));
+            if (-1 < i) {meta.javel.author.name=coverBlocks[i].slice(3)}
+            i = coverBlocks.findIndex(b=>b.startsWith('### '));
+            if (-1 < i) {meta.javel.catchCopy=coverBlocks[i].slice(4)}
+//            coverBlocks.filter(b=>!b.startsWith(`#`))
+            meta.javel.obi = coverBlocks.filter(b=>!b.match(/^#{1,6} /)); // 企画(pitch:小説の企画提案, synopsis:概要, logline: 主人公が誰を相手に、どのような動機で、何をするのかを1〜3行で表現した要約文です。作品全体の「何が起きるか」を示すあらすじの要約とも言えます。, tagline:作品の個性を際立たせる、さらに短い一文のキャッチコピーで、製品に付けられるタグ（識別票）のように、作品を一瞬で印象付ける役割を果たします。), 読者へ(catchCopy: , 帯文:売り文句(200字))
+            matches[1].index
+//            this.#set(meta, 'title', coverBlocks, '# ');
+//            this.#set(meta, 'author.name', coverBlocks, '## ');
+//            this.#set(meta, 'catchCopy', coverBlocks, '## ');
+//            const second = text.slice(match.index+1).match(/^# /);
+        } else {
+            
+        }
+        jsyaml.load();
+        const firstIdx = this._.blocks.findIndex(b=>b.startsWith('# '));
+        const secondIdx = this._.blocks.slice(firstIdx+1).findIndex(b=>b.startsWith('# ')) + firstIdx+1;
+//        console.log(firstIdx, secondIdx);
+        this._.coverRng = [firstIdx, secondIdx];
+        this._.contentRng = [secondIdx, this._.blocks.length];
+
+
+    }
+    /*
+    #set(o, name, coverBlocks, prefix='# ') {
+        let i = coverBlocks.findIndex(b=>b.startsWith(prefix));
+        if (-1 < i) {o[name] = coverBlocks[i].slice(prefix.length)}
+    }
+    */
+}
+
 window.JavelParser = JavelParser;
 })();
